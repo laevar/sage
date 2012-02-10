81296a2a0d6460fe02bc18fc412e9b91069294c4
Jochen Schulz <schulz@math.uni-goettingen.de>
2012-02-10 19:18:09 +0100

    -added some doc-files/quickref-sheets -added own quickref-sheet -began to write summaries of the units. -switched to using the sage-server sage.math instead of local sagenb-dir. -used sagetex "commandline" which is nice but now seldom used, because of the public sage-server

--- convertslides.sh
+++ convertslides.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sed -i  's/\(includegraphics.*\){\(.*\)$/\1{figures\/\2/g' $1.tex
+#sed -i  's/\(includegraphics.*\){\(.*\)$/\1{figures\/\2/g' $1.tex
 sed -i 's/begin{mupad}/begin{sage}/g' ${1}.tex
 sed -i 's/end{mupad}/end{sage}/g' ${1}.tex
 sed -i 's/end{Aufgabe}/end{aufg}/g' ${1}.tex
 sed -i 's/begin{Aufgabe}/begin{aufg}/g' ${1}.tex
+sed -i 's/end{sage}/end{sageout}/g' ${1}.tex
+sed -i 's/begin{sage}/begin{sageout}/g' ${1}.tex
+sed -i 's/end{sagein}/end{sagecommandline}/g' ${1}.tex
+sed -i 's/begin{sagein}/begin{sagecommandline}/g' ${1}.tex
